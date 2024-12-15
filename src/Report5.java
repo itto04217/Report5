d559ae7dcfbe3d9ca88ce9fd4ea4525e3da47e2d
Itto Kawamitsu <e245714@ie.u-ryukyu.ac.jp>
2024-12-16 00:18:10 +0900

    2nd commit

--- src/Report5.java
+++ src/Report5.java
@@ -2,8 +2,13 @@ import java.lang.NullPointerException;
 
 public class Report5 {
     public static void main(String args[]){
-        String str;
-        str = null;
-        System.out.println(str.length());
+        try{
+            String str;
+            str = null;
+            System.out.println(str.length());
+        }catch(NullPointerException n){
+            System.out.println("NullPointerExceptionが発生しました。");
+            System.out.println(n.getMessage());
+        }    
     }
 }
