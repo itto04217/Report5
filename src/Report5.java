7f9cc5996b049a66c319d5eb30d400a23e68303a
Itto Kawamitsu <e245714@ie.u-ryukyu.ac.jp>
2024-12-15 23:47:47 +0900

    1st commit

---
+++ src/Report5.java
@@ -0,0 +1,9 @@
+import java.lang.NullPointerException;
+
+public class Report5 {
+    public static void main(String args[]){
+        String str;
+        str = null;
+        System.out.println(str.length());
+    }
+}
